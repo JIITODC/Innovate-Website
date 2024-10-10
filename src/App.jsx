9dba5ca782bdbe68622fff1a55dd397b1a992044
TusharSin810 <tsinghal666@gmail.com>
2024-10-10 19:22:25 +0530

    Added HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Homepage from "./components/Homepage"
 
 function App() {
   return (
-    <div className='h-screen bg-gray-900'>
-      <h1 className='w-screen flex justify-center text-4xl font-extrabold bg-purple-600'>Innovate Website</h1>
-    </div>
+    <>
+      <Homepage />
+    </>
   )
 }
 
